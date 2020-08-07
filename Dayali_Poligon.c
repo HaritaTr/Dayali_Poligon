4eb10915b6fe58153f92db173dd55e1024d1f46e
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-08-07 21:06:37 +0300

    Update Dayali_Poligon.c

--- Dayali_Poligon.c
+++ Dayali_Poligon.c
@@ -9,10 +9,12 @@ int main()
 	setlocale(LC_ALL, "Turkish");
 
 	double a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, n;
-	double x1, y1, x2, y2, x3, y3, x4, y4;
+	double x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13;
 	double S1, S2, S3, S4, S5, S6, S7, S8, S9, S10;
 	double So, S11, St;
-	double Bo;
+	double Bo, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11;
+	double Bo1, Bo2, Bo3, Bo4, Bo5, Bo6, Bo7, Bo8, Bo9, Bo10, Bo11;
+	double fB, FB;
 
 		/// Bilinenleri isteyelim.
 	// Kırılma açıları; a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, n;
@@ -98,7 +100,6 @@ int main()
 	> Δxo1, Δyo1, Δxo2, Δyo2, Δxo3, Δyo3, Δxo4, Δyo4, Δxo5, Δyo5, Δxo6, Δyo6, Δxo7, Δyo7, Δxo8, Δyo8, Δxo9, Δyo9, Δxo10, Δyo10, Δxt, Δyt; */
 	/* Hesaplanan Koordinat değerleri
 	> x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13, x14, y14; */
-
 	/* POLİGON HESABI
 	Ölçülen noktaların koordinatlardan mesafelerin hesaplanması;
 	> So = Sqrt((x1 - x2)^ 2) + (y1 - y2)^ 2);
@@ -111,52 +112,87 @@ int main()
 	printf("S11 değeri: %f\n", S11);
 	printf("St değeri: %f\n", St);
 	/* 1. ve 2. noktalar arasındaki, semt açısı (Dengelenmiş başlangıç noktaları-Nirengi)
-	> βo = Atan((y2 - y1) / (x2 - x1)) * 200 / PI;
+	> Bo = atan((y2 - y1) / (x2 - x1)) * 200 / PI;
 	NOT: Açı ‘0’ dan küçük ise +200 eklenir. ‘0’dan büyük ve eşit ise açı değeri korunur. */
-	Bo = atan(((y2 - y1) / (x2 - x1)) * 200 / PI);
-	printf("Bo değeri: %f\n", Bo);
 	/* 12. ve 13. noktalar arasındaki, semt açısı (Dengelenmiş bitiş noktaları-Nirengi)
 	> β11…………. */
-
+	Bo = atan(((y2 - y1) / (x2 - x1)) * 200 / PI);
+	printf("Bo değeri: %f\n", Bo);
+	B1 = atan(((y3 - y2) / (x3 - x2)) * 200 / PI);
+	printf("B1 değeri: %f\n", B1);
+	B2 = atan(((y4 - y3) / (x4 - x3)) * 200 / PI);
+	printf("B2 değeri: %f\n", B2);
+	B3 = atan(((y5 - y4) / (x5 - x4)) * 200 / PI);
+	printf("B3 değeri: %f\n", B3);
+	B4 = atan(((y6 - y5) / (x6 - x5)) * 200 / PI);
+	printf("B4 değeri: %f\n", B4);
+	B5 = atan(((y7 - y6) / (x7 - x6)) * 200 / PI);
+	printf("B5 değeri: %f\n", B5);
+	B6 = atan(((y8 - y7) / (x8 - x7)) * 200 / PI);
+	printf("B6 değeri: %f\n", B6);
+	B7 = atan(((y9 - y8) / (x9 - x8)) * 200 / PI);
+	printf("B7 değeri: %f\n", B7);
+	B8 = atan(((y10 - y9) / (x10 - x9)) * 200 / PI);
+	printf("B8 değeri: %f\n", B8);
+	B9 = atan(((y11 - y10) / (x11 - x10)) * 200 / PI);
+	printf("B9 değeri: %f\n", B9);
+	B10 = atan(((y12 - y11) / (x12 - x11)) * 200 / PI);
+	printf("B10 değeri: %f\n", B10);
+	B11 = atan(((y13 - y12) / (x13 - x12)) * 200 / PI);
+	printf("B11 değeri: %f\n", B11);
 	/* 2. ve 3. noktalar arasındaki, açı kapanma hatası eklenmeyen semt açısı
-	> βo1 = βo + α1;
+	> Bo1 = Bo + a1;
 	NOT: Açı 200’den büyük ise 200 çıkarılır. 200’den küçük veya eşit ise açı değeri korunur. */
-
+	Bo1 = Bo + a1;
+	printf("Bo1 değeri: %f\n", Bo1);
 	/* 3. ve 4. noktalar arasındaki,Açı kapanma hatası eklenmeyen semt açısı
 	> βo2 = ………….. */
-
+	Bo2 = B1 + a2;
+	printf("Bo2 değeri: %f\n", Bo2);
 	/* 4. ve 5. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo3 = ……….. */
-
+	Bo3 = B2 + a3;
+	printf("Bo3 değeri: %f\n", Bo3);
 	/* 5. ve 6. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo4 = ………. */
-
+	Bo4 = B3 + a4;
+	printf("Bo4 değeri: %f\n", Bo4);
 	/* 6. ve 7. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo5 = ………….. */
-
+	Bo5 = B4 + a5;
+	printf("Bo5 değeri: %f\n", Bo5);
 	/* 7. ve 8. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo6 = ……………. */
-
+	Bo6 = B5 + a6;
+	printf("Bo6 değeri: %f\n", Bo6);
 	/* 8. ve 9. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo7 = ……….. */
-
+	Bo7 = B6 + a7;
+	printf("Bo7 değeri: %f\n", Bo7);
 	/* 9. ve 10. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo8 = …………….. */
-
+	Bo8 = B7 + a8;
+	printf("Bo8 değeri: %f\n", Bo8);
 	/* 10. ve 11. noktalar arasındaki, Açı kapanma hatası eklenmeyen semt açısı
 	> βo9 = ………… */
-
+	Bo9 = B8 + a9;
+	printf("Bo9 değeri: %f\n", Bo9);
 	/* 11. ve 12. noktalar arasındaki,Açı kapanma hatası eklenmeyen semt açısı
 	> βo10 = ………….. */
-
+	Bo10 = B9 + a10;
+	printf("Bo10 değeri: %f\n", Bo10);
 	/* 12. ve 13. noktalar arasındaki,Açı kapanma hatası eklenmeyen semt açısı
 	> βo11 = ……………. */
-
+	Bo11 = B10 + a11;
+	printf("Bo11 değeri: %f\n", Bo11);
 	/* Açı kapanma hatası
 	> fβ = (β11 - βo11); */
-
+	fB = (B11 - Bo11);
+	printf("Açı kapanma hatası (fB) değeri: %f\n", fB);
 	/* Açı kapanma hata sınırı
 	> Fβ = (1.5) * (Sqrt(n)); */
+	FB = (1.5) * (pow((n), 0.5));
+	printf("Açı kapanma hata sınırı (FB) değeri: %f\n", FB);
 
 	/* Açı kapanma hatası eklenen semt açıları
 	2. ve 3. noktalar arası Semt açısı
@@ -218,8 +254,8 @@ int main()
 	> x5, y5, x6, y6, x7, y7, x8, y8, x9, y9, x10, y10, x11, y11, x12, y12, x13, y13, x14, y14;
 	> x13 = x2 + Δx1;
 	> y13 = y2 + Δy1;
-	>x14 = x13 + Δx2;
-	>y14 = y13 + Δy2;
+	> x14 = x13 + Δx2;
+	> y14 = y13 + Δy2;
 	> x5 =………… */
 
 	/* Enine ve boyuna kapanma hatası
